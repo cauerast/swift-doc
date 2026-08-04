105725bde0d0cdaa38864a5d6060e8a09a15056d
cauerast <cauerast@gmail.com>
2026-08-04 02:45:05 -0300

    Funcs in Swift

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -1,3 +1,31 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+func noArgs() {
+    "noArgs"
+}
+
+func twoArgs(_ a: Int, _ b: String) {
+    "twoArgs: \(a), \(b)"
+}
+
+func twoArgsVars(a: Int, b: String) {
+    "twoArgs: \(a), \(b)"
+}
+
+twoArgs(1, "2"); // using _ pass by order
+twoArgsVars(a: 1, b: "2"); // pass defining values to compilor
+
+
+
+func sum(a: Int, b: Int) -> Int {
+    a + b // optional return (one line)
+}
+
+@discardableResult // to ignore error -> (non-used) in funcs or vars
+func sumMulti(a: Int, b: Int) -> Int {
+    let result = a + b
+    return result // needs return keyword (multi-line)
+}
+
+sum(a: 2, b: 4);
+sumMulti(a: 2, b: 8);
