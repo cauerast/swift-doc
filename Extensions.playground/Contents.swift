ee0dcb4211514d97feb3c3dec742aaf112d76d19
cauerast <cauerast@gmail.com>
2026-08-08 19:59:53 -0300

    Extensions in Swift

--- Extensions.playground/Contents.swift
+++ Extensions.playground/Contents.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+// example
 extension Int {
     func plusTwo() -> Int {
         return self + 2
@@ -8,3 +9,66 @@ extension Int {
 
 let two = 2
 two.plusTwo()
+
+
+// you can extend structs in swift
+struct Person {
+    let firtsName: String
+    let lastName: String
+}
+
+extension Person {
+    init(fullName: String){
+        let components = fullName.components(separatedBy: " ")
+        self.firtsName = components.first ?? fullName;
+        self.lastName = components.last ?? fullName;
+    }
+}
+
+let person = Person(fullName: "Foo Bar")
+person.firtsName
+person.lastName
+
+
+// you can extend a protocol
+protocol GoesVroom {
+    var vroomValue: String { get }
+    func goVroom() -> String
+}
+
+extension GoesVroom {
+    func goVroom() -> String {
+        return "\(self.vroomValue) goes vroom!"
+    }
+}
+
+struct Car {
+    let manufacturer: String
+    let model: String
+}
+
+let modelX = Car(manufacturer: "Tesla", model: "Model X")
+
+extension Car: GoesVroom {
+    var vroomValue: String {
+        return "\(self.manufacturer) \(self.model)"
+    }
+}
+
+modelX.goVroom()
+
+//extending classes with convenience initializer
+class MyDouble {
+    let value: Double
+    init(value: Double){
+        self.value = value
+    }
+}
+
+extension MyDouble {
+    convenience init (){
+        self.init(value: 0)
+    }
+}
+
+MyDouble().value;
